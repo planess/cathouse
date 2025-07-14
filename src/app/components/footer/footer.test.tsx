2df2b6fbc9460a075c07e8b8885ee91f1925530e
Kris Toff <planess.group@gmail.com>
2025-07-14 16:47:38 +0300

    test: add Footer component test (#1)

---
+++ src/app/components/footer/footer.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react'
+import Footer from './footer'
+
+test('renders current year', () => {
+  const year = new Date().getFullYear().toString()
+  render(<Footer />)
+  expect(screen.getByText(new RegExp(year))).toBeInTheDocument()
+})
