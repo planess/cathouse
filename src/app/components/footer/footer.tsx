08d907d8210dc0704334160a87ccbcbe37c537de
Kris <planess.group@protonmail.com>
2026-01-20 16:25:10 +0200

    feat: refactor Footer component to use async getTranslations and display user email

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -1,15 +1,19 @@
-import { useTranslations } from 'next-intl';
+import { getTranslations } from 'next-intl/server';
 
-export default function Footer() {
-  const t = useTranslations('footer');
+import { getUser } from '@app/hooks';
+
+export default async function Footer() {
+  const t = await getTranslations('footer');
+  const user = await getUser();
   const foundationYear = 2024;
   const org = 'Planess Group';
 
   return (
-    <footer className="bg-neutral-800 text-neutral-100 text-sm px-3 lg:px-20 py-3 flex justify-end gap-x-2 gap-y-1 transition-[padding]">
+    <footer className="bg-neutral-800 text-neutral-100 text-sm px-3 lg:px-20 py-3 flex justify-between gap-x-2 gap-y-1 transition-[padding]">
       {/* <div>
         <Link href="location">Location</Link>
       </div> */}
+      <span>{user?.email}</span>
 
       <div className="flex flex-wrap justify-end gap-x-2">
         <span>{t('tagline', { year: foundationYear })}</span>
