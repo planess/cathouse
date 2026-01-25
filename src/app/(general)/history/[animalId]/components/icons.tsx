cb0687a6932963b7a630a751be0ee5e15fb28af6
Kris <planess.group@protonmail.com>
2026-01-25 18:01:30 +0200

    feat: add fill attribute to SVG icons for consistent styling

--- src/app/(general)/history/[animalId]/components/icons.tsx
+++ src/app/(general)/history/[animalId]/components/icons.tsx
@@ -626,7 +626,11 @@ export function CreditCardSecureIcon() {
 
 export function AnalyticsIcon() {
   return (
-    <svg viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      viewBox="0 0 1920 1920"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentcolor"
+    >
       <path d="M746.667 106.667H1173.33V1493.33H746.667V106.667ZM533.333 533.333H106.667V1493.33H533.333V533.333ZM1920 1706.67H0V1824H1920V1706.67ZM1813.33 746.667H1386.67V1493.33H1813.33V746.667Z" />
     </svg>
   );
@@ -634,7 +638,11 @@ export function AnalyticsIcon() {
 
 export function GavelIcon() {
   return (
-    <svg viewBox="-2 0 19 19" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      viewBox="-2 0 19 19"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentcolor"
+    >
       <path d="M9.316 14.722a.477.477 0 0 1-.475.475H1.433a.477.477 0 0 1-.475-.475v-.863a.477.477 0 0 1 .475-.475h7.408a.476.476 0 0 1 .475.475zm-2.767-2.587a.552.552 0 0 1-.392-.163L2.96 8.776a.554.554 0 0 1 .784-.784L6.94 11.19a.554.554 0 0 1-.392.946zm7.33.992L9.435 8.682l1.085-1.084-3.173-3.173-2.97 2.97 3.173 3.172 1.102-1.101 4.445 4.445a.554.554 0 1 0 .784-.784zm-2.33-5.993a.552.552 0 0 1-.391-.162L7.96 3.775a.554.554 0 1 1 .784-.784l3.196 3.197a.554.554 0 0 1-.391.946z" />
     </svg>
   );
